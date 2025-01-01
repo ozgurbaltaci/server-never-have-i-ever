2983e4bc30c7d18524c4c43098cb366f4c807742
ozgurbaltaci <wozgurbaltaci@gmail.com>
2025-01-01 23:22:30 +0300

    vercel trying

--- api/index.js
+++ api/index.js
@@ -174,7 +174,7 @@ app.get("/api/getQuestionsCount/:roomId", async (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
